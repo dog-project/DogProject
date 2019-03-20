701a7ce97086d0e961f152c5e05c4470b24061c4
Chaviland7 <charliehaviland@gmail.com>
2019-03-20 18:26:08 -0400

    more removals

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Typography, Paper } from "@material-ui/core";
-import { withStyles, createMuiTheme } from "@material-ui/core/styles";
-import red from "@material-ui/core/colors/red";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   root: {
