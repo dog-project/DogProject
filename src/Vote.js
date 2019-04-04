03d27cc4dbb73cbdd51c599852a4cae4370c0151
Chaviland7 <charliehaviland@gmail.com>
2019-04-04 09:44:51 -0400

    fixing typo

--- src/Vote.js
+++ src/Vote.js
@@ -162,7 +162,7 @@ class Vote extends Component {
         <Paper className={classes.paper}>
           <Typography variant="h3" style={{paddingBottom: "7px"}}>Vote Page</Typography>
           <p>Please select the corresponding radio button for which dog you think is cuter. If you have an indifferent view of which dog is cuter, please select "I am indifferent".</p>
-          <p>When you click "Submit", you will be shown a new pair of dogs. You can vote as many times as you would like until you have seen all 325 possible pairing of the 26 dogs.</p>
+          <p>When you click "Submit", you will be shown a new pair of dogs. You can vote as many times as you would like until you have seen all 325 possible pairings of the 26 dogs.</p>
         </Paper>
         <Paper className={classes.paper}>
           <p>{voteCount}/325</p>
