6896ad0be02434f541843474276826f3feb01792
merkovitz <merkovitz.s@husky.neu.edu>
2019-05-22 16:20:57 -0400

    front page updates for ethics conference

--- src/Home.js
+++ src/Home.js
@@ -40,32 +40,37 @@ function Home(props) {
           </Typography>
 
           <p>
-            We are a student research group in the Philosophy & Religion
-            Department at Northeastern University that conducted a project to
-            determine who in the Department has the cutest dog, a topic about
-            which there has been long-standing and passionate disagreement.
-            Instead of arguing until the end of time (as philosophers will do),
-            three professors decided to set their differences aside and let us
-            try to answer the question in a philosophically and scientifically
-            rigorous way. This was the origin of the Cute Dog Project.
+            We are an undergraduate student research group in the Philosophy &
+            Religion Department at Northeastern University that conducted a
+            project to determine who in the Department has the cutest dog, a
+            topic about which there has been long-standing and passionate
+            disagreement. Instead of arguing until the end of time, three
+            professors decided to set their differences aside and let us try to
+            answer the question in a philosophically and scientifically rigorous
+            way. This was the origin of the Cute Dog Project.
           </p>
           <p>
-            One reputable method of settling disputes is to vote. We decided
-            that a voting contest should be put in place to settle whose dog is
-            cutest in the entirety of the Philosophy & Religion Department
-            (students, faculty and staff) at Northeastern. Eventually, we hope
-            to extend this to the entire university and beyond. However,
-            answering the apparently simple question of "Who has the cutest
-            dog?" actually requires answers to several other difficult
-            questions, such as:
+            One reputable method of settling disputes in judgement is to vote.
+            We decided to run a vote to settle whose dog is cutest in the
+            Philosophy & Religion Department (students, faculty and staff) at
+            Northeastern. However, answering the deceptively simple question of
+            "Who has the cutest dog?" actually requires answers to several other
+            difficult questions in the areas of democracy, ethics, and social
+            choice, such as:
           </p>
           <ul>
             <li>
-              How are democratic processes used in group decisions to resolve
-              disputes?
+              How can democratic processes be used fairly to resolve disputes?
             </li>
             <li>What would our voting procedure be?</li>
-            <li>How do we define cuteness and can we even define it?</li>
+            <li>
+              How do we balance user accessibility, voter anonymity, and
+              security in an online voting system?
+            </li>
+            <li>
+              How do we define cuteness, and what voting mechanisms work to
+              identify that definition?
+            </li>
             <li>
               Who would be allowed to submit photos and who would be allowed to
               vote?
@@ -74,10 +79,13 @@ function Home(props) {
               How does this question help us uncover answers about generalized
               social choice theories and voting procedures?
             </li>
-            <li>How do we control for possible social and cultural biases?</li>
             <li>
-              How do we collect and manage voting data in ways that are secure
-              and respect privacy?
+              How do we control for possible social and cultural biases in a way
+              that allows us to accurately represent people’s true preferences?
+            </li>
+            <li>
+              How do we collect and manage voting data in ways that are secure,
+              ethical, and respect the privacy of the voters?
             </li>
           </ul>
           <Grid container spacing={24}>
@@ -85,22 +93,24 @@ function Home(props) {
               <p>
                 Determining whose dog is the cutest required us to design a
                 system informed by democratic theory, social choice theory,
-                value theory, critical theory, philosophy of science, and
-                information ethics. We had to grapple with the same sorts of
+                information ethics, value theory, critical theory, and
+                philosophy of science. We had to grapple with the same sorts of
                 issues that arise in democratic practice and social
                 decision-making in other contexts – for example, selecting a
                 presidential candidate from a crowded primary field or selecting
                 applicants for admission to a competitive university.
               </p>
               <p>
-                We have learned alot along the way, but have now completed the
-                contest. The winner of the contest was Pearl!
+                Voting was open from April 3 to April 22, where almost 60,000
+                votes were submitted by almost 1,000 unique voters. After the
+                contest, we used a social choice method called Ranked Pairs to
+                identify the winner. The winner of the contest was Pearl!
               </p>
             </Grid>
-            <Grid sm={12} md={6} >
+            <Grid sm={12} md={6}>
               <img
                 src="images/dogs/57.png"
-                className={ classes.winner }
+                className={classes.winner}
                 alt="winner"
               />
               <Typography variant="p" style={{ textAlign: "center" }}>
@@ -108,12 +118,12 @@ function Home(props) {
               </Typography>
             </Grid>
             <Grid item sm={12}>
-            <Typography variant="p">
-              This website details our contest, our methods, results, and the
-              many lessons we've learned along the way. Please explore the site
-              to learn more about or project, see the full results, and see what
-              cute dogs can teach us about democracy.
-            </Typography>
+              <Typography variant="p">
+                This website details our contest, our methods, results, and the
+                many lessons we've learned along the way. Please explore the
+                site to learn more about or project, see the full results, and
+                see what cute dogs can teach us about democracy.
+              </Typography>
             </Grid>
           </Grid>
         </Paper>
